6319043a0d60ddfb18facf143edf8c00947114ed
vanessaperdomo <p.laurav2@gmail.com>
2026-05-07 23:21:22 -0500

    HU-05 password recovery

--- web-renta-movil/src/routes/AppRouter.jsx
+++ web-renta-movil/src/routes/AppRouter.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import LandingPage  from '../modules/landing/LandingPage'
-import RegistroPage from '../modules/auth/pages/RegistroPage'
-import LoginPage    from '../modules/auth/pages/LoginPage'
+import LandingPage             from '../modules/landing/LandingPage'
+import RegistroPage            from '../modules/auth/pages/RegistroPage'
+import LoginPage               from '../modules/auth/pages/LoginPage'
+import RecuperarContrasenaPage from '../modules/auth/pages/RecuperarContrasenaPage'
+import NuevaContrasenaPage     from '../modules/auth/pages/NuevaContrasenaPage'
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"         element={<LandingPage />} />
-        <Route path="/registro" element={<RegistroPage />} />
-        <Route path="/login"    element={<LoginPage />} />
-        
-        {/* Redirección por si escriben cualquier cosa rara */}
+        <Route path="/"                  element={<LandingPage />} />
+        <Route path="/registro"          element={<RegistroPage />} />
+        <Route path="/login"             element={<LoginPage />} />
+        <Route path="/recuperar"         element={<RecuperarContrasenaPage />} />
+        <Route path="/nueva-contrasena"  element={<NuevaContrasenaPage />} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
