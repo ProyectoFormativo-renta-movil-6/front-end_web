605a0b05e06ca594c785f5e625e0408cee31b426
vanessaperdomo <p.laurav2@gmail.com>
2026-05-13 14:40:22 -0500

    cambio de idioma y tema

--- web-renta-movil/src/main.jsx
+++ web-renta-movil/src/main.jsx
@@ -1,22 +1,23 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
+import { LandingProvider } from './modules/landing/LandingContext.jsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: {
-      retry: 1,
-      staleTime: 1000 * 60 * 5, // 5 minutos
-    },
+    queries: { retry: 1, staleTime: 1000 * 60 * 5 },
   },
 })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <LandingProvider>
+        <App />
+      </LandingProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
